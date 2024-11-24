26488cb0bcd4c1eef322073748ea442daae2d7b9
Zhong <zhtgeneral@gmail.com>
2024-11-24 13:36:41 -0800

    refactor

--- app/(main)/newPost.tsx
+++ app/(main)/newPost.tsx
@@ -8,16 +8,34 @@ import { theme } from '@/constants/theme'
 import { useAuth } from '@/context/AuthContext'
 import { hp, wp } from '@/helpers/common'
 import { getSupabaseFileUrl } from '@/services/imageServices'
+import { User } from '@/types/supabase'
 import { ResizeMode, Video } from 'expo-av'
 import { Image } from 'expo-image'
 import * as ImagePicker from 'expo-image-picker'
 import { ImagePickerAsset } from 'expo-image-picker'
 import type { MediaType } from 'expo-image-picker/src/ImagePicker.types'
 import { useRouter } from 'expo-router'
-import React, { useRef, useState } from 'react'
+import React, { Dispatch, SetStateAction, useRef, useState } from 'react'
 import { Pressable, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 
+interface NewPostHeaderProps {
+  user: User
+}
+interface NewPostUploadMediaProps {
+  setFile: Dispatch<SetStateAction<ImagePickerAsset | null>>
+}
+interface NewPostDisplayMediaProps {
+  file: ImagePickerAsset | null,
+  setFile: Dispatch<SetStateAction<ImagePickerAsset | null>>
+}
 
+/**
+ * This page handles creating a post.
+ * 
+ * It displays the header.
+ * It displays the text editor allowing the user to enter rich text.
+ * It displays an input to add media with images or videos.
+ */
 const NewPost = () => {
   const { user } = useAuth();
   const bodyRef = useRef("");
@@ -26,50 +44,8 @@ const NewPost = () => {
   const [loading, setLoading ] = useState(false);
   const [file, setFile] = useState<ImagePickerAsset | null>(null);
 
-  async function onPick(isImage: boolean) {
-    let mediaConfig = {
-      mediaTypes: "images" as MediaType,
-      allowsEditing: true,
-      aspect: [3, 3] as [number, number],
-      quality: 0.5,
-    }
-    if (!isImage) {
-      mediaConfig = {
-        ...mediaConfig,
-        mediaTypes: 'videos' as MediaType
-      }
-    }
-    const result = await ImagePicker.launchImageLibraryAsync(mediaConfig);
-    console.log('file selected NewPost::onPick: ', JSON.stringify(result, null, 2));
-    if (!result.canceled) {
-      setFile(result.assets[0]);
-    }
-  }
-
   async function onSubmit() {
 
-  }
-  function isLocalFile(file: ImagePickerAsset) {
-    return typeof file === 'object';
-  }
-
-  function getFileType(file: ImagePickerAsset | null) {
-    if (!file) {
-      return null;
-    }
-    if (isLocalFile(file)) {
-      return file.type;
-    }
-  }
-
-  function getFileUri(file: ImagePickerAsset | null): string {
-    if (!file) {
-      return "";
-    }
-    if (isLocalFile(file)) {
-      return file.uri;
-    }
-    return getSupabaseFileUrl(file.uri)?.uri || "";
   }
 
   return (
@@ -77,81 +53,14 @@ const NewPost = () => {
       <View style={styles.container}>
         <Header title="Create post" />
         <ScrollView contentContainerStyle={{ gap: 20 }}>  
-          {/* Header */}
-            <View style={styles.header}> 
-              <Avatar 
-                uri={user.image}
-                size={hp(6.6)}
-                rounded={theme.radius.xl}
-              />
-              <View style={{ gap: 2 }}> 
-                <Text style={styles.username}>
-                  {user && user.name}
-                </Text>
-                <Text style={styles.publicText}>
-                  public
-                </Text>
-              </View>
-            </View>
-          {/* Body */}
+          <NewPostHeader user={user} />
           <View style={styles.body} >
-            {/* Text editor */}
-            <View>
-              <RichTextEditor 
-                editorRef={editorRef}
-                onChange={(body: string) => bodyRef.current = body}
-              />
-            </View>
-            {/* Displayed media */}
-            {file && (
-              <View style={styles.file}>
-                {getFileType(file) === 'video'? (
-                  <Video 
-                    style={{ flex: 1 }}
-                    source={{ uri: getFileUri(file) }}
-                    useNativeControls={true}
-                    resizeMode={ResizeMode.COVER}
-                    isLooping={true}
-                  />
-                ): (
-                  <Image 
-                    source={{ uri: getFileUri(file) }}
-                    contentFit='cover'
-                    style={{ flex: 1 }}
-                  />
-                )}
-                <Pressable style={styles.closeIcon} onPress={() => setFile(null)}>
-                  <Icon 
-                    name="delete" 
-                    size={20} 
-                    stroke="white" 
-                  />
-                </Pressable>
-              </View>
-            )}
-            {/* Upload media */}
-            <View style={styles.media}>
-              <Text style={styles.addImageText}>
-                Add to your post
-              </Text>
-              <View style={styles.mediaIcons}>
-                <TouchableOpacity onPress={() => onPick(true)}>
-                  <Icon 
-                    name='image' 
-                    size={30} 
-                    stroke={theme.colors.dark} 
-                  />
-                </TouchableOpacity>
-                <TouchableOpacity onPress={() => onPick(false)}>
-                  <Icon 
-                    name='video' 
-                    size={35} 
-                    strokeWidth={1.7}
-                    stroke={theme.colors.dark} 
-                  />
-                </TouchableOpacity>
-              </View>
-            </View>
+            <RichTextEditor 
+              editorRef={editorRef}
+              onChange={(body: string) => bodyRef.current = body}
+            />
+            <NewPostDisplayMedia file={file} setFile={setFile}/>
+            <NewPostUploadMedia setFile={setFile} />
           </View>
         </ScrollView>
         <Button 
@@ -166,7 +75,148 @@ const NewPost = () => {
   )
 }
 
-export default NewPost
+/**
+ * This component handles displaying the selected media.
+ * 
+ * For images, it displays the image.
+ * For video, it displays the video player that loops the video.
+ * 
+ * It displays a close icon that closes the display media when pressed.
+ */
+const NewPostDisplayMedia: React.FC<NewPostDisplayMediaProps> = ({
+  file, 
+  setFile
+}) => {
+  function isLocalFile(file: ImagePickerAsset) {
+    return typeof file === 'object';
+  }
+  function getFileType(file: ImagePickerAsset | null) {
+    if (!file) {
+      return null;
+    }
+    if (isLocalFile(file)) {
+      return file.type;
+    }
+  }
+  function getFileUri(file: ImagePickerAsset | null): string {
+    if (!file) {
+      return "";
+    }
+    if (isLocalFile(file)) {
+      return file.uri;
+    }
+    return getSupabaseFileUrl(file.uri)?.uri || "";
+  }
+
+  if (!file) {
+    return null;
+  }
+  return (
+    <View style={styles.file}>
+      {getFileType(file) === 'video'? (
+        <Video 
+          style={{ flex: 1 }}
+          source={{ uri: getFileUri(file) }}
+          useNativeControls={true}
+          resizeMode={ResizeMode.COVER}
+          isLooping={true}
+        />
+      ): (
+        <Image 
+          source={{ uri: getFileUri(file) }}
+          contentFit='cover'
+          style={{ flex: 1 }}
+        />
+      )}
+      <Pressable style={styles.closeIcon} onPress={() => setFile(null)}>
+        <Icon 
+          name="delete" 
+          size={20} 
+          stroke="white" 
+        />
+      </Pressable>
+    </View>
+  );
+}
+
+/**
+ * This component handles allowing the user to upload media.
+ * It allows the user to upload 1 image or video.
+ */
+const NewPostUploadMedia: React.FC<NewPostUploadMediaProps> = ({
+  setFile
+}) => {
+  async function onPick(isImage: boolean) {
+    let mediaConfig = {
+      mediaTypes: "images" as MediaType,
+      allowsEditing: true,
+      aspect: [3, 3] as [number, number],
+      quality: 0.5,
+    }
+    if (!isImage) {
+      mediaConfig = {
+        ...mediaConfig,
+        mediaTypes: 'videos' as MediaType
+      }
+    }
+    const result = await ImagePicker.launchImageLibraryAsync(mediaConfig);
+    if (!result.canceled) {
+      setFile(result.assets[0]);
+    }
+  }
+  return (
+    <View style={styles.media}>
+      <Text style={styles.addImageText}>
+        Add to your post
+      </Text>
+      <View style={styles.mediaIcons}>
+        <TouchableOpacity onPress={() => onPick(true)}>
+          <Icon 
+            name='image' 
+            size={30} 
+            stroke={theme.colors.dark} 
+          />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => onPick(false)}>
+          <Icon 
+            name='video' 
+            size={35} 
+            strokeWidth={1.7}
+            stroke={theme.colors.dark} 
+          />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+}
+
+/**
+ * This component handles rendering the user.
+ * It displays the user's name and profile picture.
+ */
+const NewPostHeader: React.FC<NewPostHeaderProps> = ({
+  user
+}) => {
+  return (
+    <View style={styles.header}> 
+      <Avatar 
+        uri={user.image}
+        size={hp(6.6)}
+        rounded={theme.radius.xl}
+      />
+      <View style={{ gap: 2 }}> 
+        <Text style={styles.username}>
+          {user && user.name}
+        </Text>
+        <Text style={styles.publicText}>
+          public
+        </Text>
+      </View>
+    </View>
+  );
+}
+
+export default NewPost;
 
 const styles = StyleSheet.create({
   container: {
